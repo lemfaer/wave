7ba91c2b557bce977848ae5d48cea3cae1366706
lem <lemfaer@gmail.com>
2017-05-09 14:49:20 +0300

    min

--- app/handler/filter.py
+++ app/handler/filter.py
@@ -1,6 +1,6 @@
 _filter = filter
 
 def filter(state):
-	filt = lambda item: item["rank"] > 0
+	filt = lambda item: item["rank"] > state["setup"]["min"]
 	item = _filter(filt, state["tmp"]["items"])
 	state["tmp"]["items"] = list(item)
